6772edebfb6240695cdf8e8e8c56d96ea46b21c3
Kauanny19 <159188864+Kauanny19@users.noreply.github.com>
2024-03-25 15:16:32 -0300

    Exercicios 1 e 2 - OP Arit. e PAR

---
+++ Kauanny_Python_25-03/exercicio_OP_Art.py
@@ -0,0 +1,10 @@
+numero1 = 65
+numero2 = 9
+
+print("Adição:", numero1 + numero2)
+print("Subtração:", numero1 - numero2)
+print("Multiplicação:", numero1 * numero2)
+print("Divisão:", numero1 / numero2)
+print("Divisão inteira:", numero1 // numero2)
+print("Resto da divisão:", numero1 % numero2)
+print("Potência:", numero1 ** numero2)
---
+++ Kauanny_Python_25-03/exercicio_PAR.py
@@ -0,0 +1,11 @@
+
+numero = int(input("Digite um número positivo: "))
+
+while(numero < 0):
+   numero = int(input("Digite um número positivo: "))
+ 
+if numero % 2 == 0:
+ print(' ""P-A-R!!!"" ')
+
+else:
+ print("Tente outra vez")
